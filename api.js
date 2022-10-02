a9fd08e1d1ca568484e1ca5f7066b2b5d5a0b756
Benjamin Pla <benjamin.pla@visma.com>
2022-10-02 13:52:23 +0200

    add "express-sanitizer"

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import router from './db/routes/index.js'
+import expressSanitizer from 'express-sanitizer'
 
 const api = express()
 api.use(express.json())
+api.use(expressSanitizer())
 api.use('/', router)
 
 export default api;
